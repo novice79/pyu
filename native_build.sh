e66ee01542b449d42af3463de5b85cb3e0524453
jiangwei <wwpptogether@gmail.com>
2023-05-03 22:07:13 +0800

    change sh get ostype method

--- native_build.sh
+++ native_build.sh
@@ -3,45 +3,44 @@ set -e
 
 scirptName=$0
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        PRBUILT="/data/clib-prebuilt/linux"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-        PRBUILT="$HOME/clib-prebuilt/macos"
-elif [[ "$OSTYPE" == "win32" ]]; then
-    :
+ostype=$(uname)
+if [[ "$ostype" == "Linux"* ]]; then
+    PRBUILT="/data/clib-prebuilt/linux/x86_64"
+elif [[ "$OSTYPE" == "Darwin"* ]]; then
+    PRBUILT="$HOME/clib-prebuilt/macos"
 else
     :
 fi
-dir="$PWD/_build/$OSTYPE"
-examplePrefix="$PWD/dist/$OSTYPE"
+dir="$PWD/_build"
+examplePrefix="$PWD/dist"
 PREFIX=${prefix:-$examplePrefix}
 while [ $# -gt 0 ]; do
-  case "$1" in
-    -prefix=* | --PREFIX=* | prefix=* | PREFIX=*)
-      PREFIX="${1#*=}"
-      ;;
-    -h | --help)
-      echo "build example app(s) and install lib to target dir with:"
-      echo "$scirptName prefix=\$targetDir"
-      echo "default lib installation dir: $examplePrefix --if without prefix provided"
-      echo "example app always install to $examplePrefix "
-      exit 0
-      ;;
-    lib)
-      LIB_ONLY=true
-      ;;
-    *)
-      printf "remove $dir for clean build\n"
-      rm -rf $dir
-  esac
-  shift
+    case "$1" in
+        -prefix=* | --PREFIX=* | prefix=* | PREFIX=*)
+            PREFIX="${1#*=}"
+        ;;
+        -h | --help)
+            echo "build example app(s) and install lib to target dir with:"
+            echo "$scirptName prefix=\$targetDir"
+            echo "default lib installation dir: $examplePrefix --if without prefix provided"
+            echo "example app always install to $examplePrefix "
+            exit 0
+        ;;
+        lib)
+            LIB_ONLY=true
+        ;;
+        *)
+            printf "remove $dir for clean build\n"
+            rm -rf $dir
+    esac
+    shift
 done
 
 # build http lib
 cmake -GNinja -H"src" -B"$dir/lib" \
 -DCMAKE_FIND_ROOT_PATH="$PRBUILT" \
 -DCMAKE_INSTALL_PREFIX=$PREFIX \
--DCMAKE_BUILD_TYPE=Release 
+-DCMAKE_BUILD_TYPE=Release
 
 cmake --build "$dir/lib"
 cmake --install "$dir/lib"
@@ -50,7 +49,7 @@ cmake --install "$dir/lib"
 cmake -GNinja -H"examples" -B$dir \
 -DCMAKE_FIND_ROOT_PATH="$PREFIX;$PRBUILT" \
 -DCMAKE_INSTALL_PREFIX=$examplePrefix \
--DCMAKE_BUILD_TYPE=Release 
+-DCMAKE_BUILD_TYPE=Release
 
 cmake --build $dir
 cmake --install $dir
